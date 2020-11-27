4803d663f89231e6b81f56da74c768fb93ae3663
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-11-27 15:01:35 -0800

    added JSON files and other dependencies

---
+++ EmployeeSchema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS EmployeeDB;
+CREATE database EmployeeDB;
+
+USE EmployeeDB;
+
+CREATE TABLE top5000 (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE top_albums (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+SELECT * FROM top5000;
+select * from top_albums;
